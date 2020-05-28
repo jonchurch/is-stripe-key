28dc7eec0ebe04969abbc8bf5be117fc7805fe61
Jon Church <me@jonchurch.com>
2020-05-28 16:39:45 -0400

    change how patterns are created, add contains function

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const exactMatch = /(pk|sk)_(test|live)_[A-Za-z0-9]{24}/;
-const looseMatch = /(pk|sk)_(test|live)_[A-Za-z0-9]+/;
+const basePattern = '(pk|sk)_(test|live)_[A-Za-z0-9]';
+
+const exactMatch = new RegExp(`${basePattern}{24}`); // /(pk|sk)_(test|live)_[A-Za-z0-9]{24}/;
+const looseMatch = new RegExp(`${basePattern}+`); // /(pk|sk)_(test|live)_[A-Za-z0-9]+/;
 
 function isStripeKey(string = '', { exact = true } = {}) {
-  const reg = exact ? exactMatch : looseMatch;
+  const pattern = exact ? exactMatch : looseMatch;
+  const reg = new RegExp(`^${pattern}$`);
+  return reg.test(string.trim());
+}
+
+function containsStripeKey(string = '', { exact = true } = {}) {
+  const pattern = exact ? exactMatch : looseMatch;
+  const reg = new RegExp(pattern, 'g');
+
   return reg.test(string.trim());
 }
 module.exports = isStripeKey;
+
+const key = 'sk_live_vQp5vQIibsjlRWJm5nkMobJW00K8o8uW7q';
+
+console.log(isStripeKey(`hello${key}`));
