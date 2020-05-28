5e002d64ef05fb60d71188a03bf3bae50f06c001
Jon Church <me@jonchurch.com>
2020-05-28 16:13:48 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const exactMatch = /(pk|sk)_(test|live)_[A-Za-z0-9]{24}/;
+const looseMatch = /(pk|sk)_(test|live)_[A-Za-z0-9]+/;
+
+function isStripeKey(string = '', { exact = true } = {}) {
+  const reg = exact ? exactMatch : looseMatch;
+  return reg.test(string);
+}
+module.exports = isStripeKey;
